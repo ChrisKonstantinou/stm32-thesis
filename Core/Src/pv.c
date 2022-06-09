48e0df2953cd3ece037283491dec69c50df9e1a3
Chris Konstantinou <chriskon97@gmail.com>
2022-06-09 17:27:03 +0300

    Create pv.c

---
+++ Core/Src/pv.c
@@ -0,0 +1,17 @@
+#include "pv.h"
+
+
+void PVModelInit(PVpanel *panel)
+{
+
+}
+
+float PVModelGetCurrentFromVoltage(PVpanel *panel, float voltage)
+{
+	return -1;
+}
+
+void PVModelUpdateModelRealTime(PVpanel *old_panel, PVpanel *new_panel)
+{
+
+}
