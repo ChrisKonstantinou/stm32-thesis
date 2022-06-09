0d680d96c105a20f52c56352a7ba307e833e0782
Chris Konstantinou <chriskon97@gmail.com>
2022-06-09 17:27:06 +0300

    Create pwm_control.c

---
+++ Core/Src/pwm_control.c
@@ -0,0 +1,14 @@
+#include "pwm_control.h"
+#include "pid.h"
+
+PIDController *controler;
+
+
+uint16_t PWM_GenerateControlSignal(uint16_t input_data)
+{
+	HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
+
+
+
+	return 50;
+}
