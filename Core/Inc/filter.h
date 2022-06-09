d73d18769dbf73bce57e6865ad77b6e1c85e5666
Chris Konstantinou <chriskon97@gmail.com>
2022-06-09 17:26:40 +0300

    Create filter.h

---
+++ Core/Inc/filter.h
@@ -0,0 +1,17 @@
+#ifndef __FILTER_C
+#define __FILTER_C
+
+typedef struct
+{
+
+} LPassFilter;
+
+typedef struct
+{
+
+} HPassFilter;
+
+void LPassFilterInit(LPassFilter *);
+void HPassFilterInit(HPassFilter *);
+
+#endif
